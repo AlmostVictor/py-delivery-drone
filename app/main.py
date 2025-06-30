4f236be07926af20988884d4b46a685b37adfdfe
AlmostVictor <zhovtanetskyj.victor@gmail.com>
2025-06-30 20:57:09 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -34,7 +34,7 @@ class FlyingRobot(BaseRobot):
         if coords is None:
             coords = [0, 0, 0]
         elif len(coords) == 2:
-            coords.append(0)
+            coords = coords + [0]
         super().__init__(name, weight, coords)
 
     def go_up(self, step: int = 1) -> None:
